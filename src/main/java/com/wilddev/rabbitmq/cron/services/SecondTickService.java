e9c67610808b884d1b94201be6635bcf579f47a1
WildDev <georgy@wilddev.email>
2024-09-29 23:38:05 +0300

    Elimination of inaccuracies

--- src/main/java/com/wilddev/rabbitmq/cron/services/SecondTickService.java
+++ src/main/java/com/wilddev/rabbitmq/cron/services/SecondTickService.java
@@ -2,6 +2,7 @@ package com.wilddev.rabbitmq.cron.services;
 
 import com.wilddev.rabbitmq.cron.config.settings.ExchangeSettings;
 import com.wilddev.rabbitmq.cron.config.settings.RoutingKeySettings;
+
 import lombok.extern.slf4j.Slf4j;
 
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
